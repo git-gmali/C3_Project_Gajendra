0b6c32af6559f5c7fa8e61d9e27ce8ccb13d8f35
Gajendra Mali <>
2021-05-04 19:14:05 +0530

    Part 2: Implementing Methods and Tests
    Restaurant.java - getMenu(), isRestaurantOpen()
    RestaurantService.java - findRestaurantByName()
    RestaurantServiceTest.java - searching_for_existing_restaurant_should_return_expected_restaurant_object(), searching_for_non_existing_restaurant_should_throw_exception()
    RestaurantTest.java - Is_restaurant_open_should_return_true_if_time_is_between_opening_and_closing_time(), Is_restaurant_open_should_return_false_if_time_is_outside_opening_and_closing_time()
    Code refatored

---
+++ Restaurant_project -Student version/src/main/java/Restaurant.java
@@ -0,0 +1,79 @@
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Restaurant {
+  private String name;
+  private String location;
+  public LocalTime openingTime;
+  public LocalTime closingTime;
+  private List<Item> menu = new ArrayList<Item>();
+
+  public Restaurant(String name, String location, LocalTime openingTime, LocalTime closingTime) {
+    this.name = name;
+    this.location = location;
+    this.openingTime = openingTime;
+    this.closingTime = closingTime;
+  }
+
+  public boolean isRestaurantOpen() {
+
+    // Get the current time
+    // Check if current time is within the limits os opening time and closing time to declare
+    // that the restaurant is open.
+    LocalTime currentTime = this.getCurrentTime();
+    long secondsRemainingtoClose = ChronoUnit.SECONDS.between(currentTime, closingTime);
+    long secondsPassedOpeningTime = ChronoUnit.SECONDS.between(openingTime, currentTime);
+
+    // System.out.println(openingTime + " " + currentTime + " " + closingTime + " "
+    // + minutesRemainingtoClose + " " + minutesPassedOpeningTime);
+    if (secondsPassedOpeningTime >= 0 && secondsRemainingtoClose >= 0)
+      return true;
+
+    return false;
+  }
+
+  public LocalTime getCurrentTime() {
+    return LocalTime.now();
+  }
+
+  public List<Item> getMenu() {
+    // Get the object of unmodifiable list to make sure the list is not modified outside.
+    return Collections.unmodifiableList(menu);
+  }
+
+  private Item findItemByName(String itemName) {
+    for (Item item : menu) {
+      if (item.getName().equals(itemName))
+        return item;
+    }
+    return null;
+  }
+
+  public void addToMenu(String name, int price) {
+    Item newItem = new Item(name, price);
+    menu.add(newItem);
+  }
+
+  public void removeFromMenu(String itemName) throws itemNotFoundException {
+
+    Item itemToBeRemoved = findItemByName(itemName);
+    if (itemToBeRemoved == null)
+      throw new itemNotFoundException(itemName);
+
+    menu.remove(itemToBeRemoved);
+  }
+
+  public void displayDetails() {
+    System.out.println("Restaurant:" + name + "\n" + "Location:" + location + "\n" + "Opening time:"
+        + openingTime + "\n" + "Closing time:" + closingTime + "\n" + "Menu:" + "\n" + getMenu());
+
+  }
+
+  public String getName() {
+    return name;
+  }
+
+}
